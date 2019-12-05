29ba28d41ef0df296d6313b99eada159ce2c4c86
Jake <jaka.presecnik@gmail.com>
2019-12-05 12:17:33 +0100

    Made the code a bit more readible

--- website/app.js
+++ website/app.js
@@ -2,26 +2,28 @@
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const apiKey = '&APPID=69f7a79ea60d1f056d03a841887913dc';
 // Event listener on the button
-document.getElementById('generate').addEventListener('click', submitEntry);
+document.getElementById('generate').addEventListener('click', performAction);
 //(3) Function called by event listener that does all the application
-function submitEntry(e) {
+function performAction(e) {
   // Variables that get the data needed for application to work:
   let zip = document.getElementById('zip').value;
   let country = ',' + document. getElementById('country').value;
-  //asynchronous function to get the data from openweathermap with parameters entered by user
-  const getData = async(baseURL, zip, country, apiKey) => {
-    const res = await fetch (baseURL+zip+country+apiKey)
-    try {
-      const data = await res.json();
-      console.log(data);
-      return data;
-    }catch (error) {
-      console.log('error', error);
-    }
-  }
   getData (baseURL, zip, country, apiKey);
 }
-// Function for getting data on submit DONE
+// Function for event listener DONE
+
+//asynchronous function to get the data from openweathermap with parameters entered by user
+const getData = async(baseURL, zip, country, apiKey) => {
+  const res = await fetch (baseURL+zip+country+apiKey)
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  }catch (error) {
+    console.log('error', error);
+  }
+}
+// DONE - *(if error reset the function to be before performAction)*
 
 // Create a new date instance dynamically with JS
  let d = new Date();
